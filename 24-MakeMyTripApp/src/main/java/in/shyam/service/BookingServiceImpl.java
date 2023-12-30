99ba489d1aff25833ed59cc8170ebfcc3d399076
Shyam Kumar <65010740+shyamk5@users.noreply.github.com>
2023-12-30 20:19:37 +0530

    WebFlux

--- 24-MakeMyTripApp/src/main/java/in/shyam/service/BookingServiceImpl.java
+++ 24-MakeMyTripApp/src/main/java/in/shyam/service/BookingServiceImpl.java
@@ -3,6 +3,7 @@ package in.shyam.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import in.shyam.request.Passenger;
 import in.shyam.response.Ticket;
@@ -15,18 +16,50 @@ public class BookingServiceImpl implements BookingServices {
 	
 	@Override
 	public Ticket bookTicket(Passenger passenger) {
+		
+		//Method 1: Rest Template
+		/*
 		RestTemplate rt = new RestTemplate();
 		ResponseEntity<Ticket> respTicket = rt.postForEntity(BOOK_TICKET_URL, passenger, Ticket.class);
 		Ticket t = respTicket.getBody();
 		return t;
+		*/
+		
+		
+		//Method 2: Web Starter Flux
+		WebClient webClient = WebClient.create();
+		Ticket ticket = webClient.post()
+								 .uri(BOOK_TICKET_URL)
+								 .bodyValue(passenger)
+								 .retrieve()
+								 .bodyToMono(Ticket.class) 
+								 .block(); //sync call
+		
+		return ticket;
+		
 	}
 
 	@Override
 	public Ticket getTicket(Integer pnrNum) {
+		
+		//Method 1: Rest Template
+		/*
 		RestTemplate rt = new RestTemplate();
 		ResponseEntity<Ticket> respTicket = rt.getForEntity(GET_TICKET_URL, Ticket.class, pnrNum);
 		Ticket t = respTicket.getBody();
 		return t;
+		*/
+		
+		
+		//Method 2: Web Starter Flux
+		WebClient webClient = WebClient.create();
+		Ticket ticket = webClient.get()
+								 .uri(GET_TICKET_URL, pnrNum)
+								 .retrieve()
+								 .bodyToMono(Ticket.class)
+								 .block(); //sync
+		
+		return ticket;
 	}
 	
 }
